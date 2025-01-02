c0a7980e3a54323eda9f69602baeb2953858fb0f
Eric George <erickurian@gmail.com>
2025-01-01 20:17:22 -0700

    probably concluded probabilities for cards

--- PackOpeningSimulator/opener.py
+++ PackOpeningSimulator/opener.py
@@ -27,6 +27,20 @@ password = os.getenv("DB_PASSWORD")
 # Sum total = 60 + 12 + 6 + 4 + 1 = 83
 
 # reconfigured rarities for 'greater enjoyment'
+# SP = RRR
+
+# EACH PACK -> 7 CARDS
+# EACH BOX -> 12 PACKS
+# EACH BOX -> 84
+# 84:
+# 42 C : 24 R : 12 RR : 6 RRR
+    #  66 ->   78  -> 84
+# Average probabilities per rarity:
+    # C: 50%     -> 7/14 -> 3.5/7
+    # R: 28.57%  -> 4/14 ->   2/7
+    # RR: 14.29% -> 2/14 ->   1/7
+    # RRR: 7.14% -> 1/14 -> 0.5/7
+    
 # 7 cards per pack
 # 12 packs per box
 # 3 C per pack
@@ -69,17 +83,11 @@ def open_pack():
                     card_rarity = R
                 case x if x < 85:
                     card_rarity = RR
-                case x if x < 99.5:
-                    card_rarity = RRR
                 case _:
-                    card_rarity = SP
+                    card_rarity = RRR
             pack_rarities.append(card_rarity)
         rarity = random.uniform(0, 100)
-        card_rarity = -1
-        if rarity < 75:
-            card_rarity = RRR
-        else:
-            card_rarity = SP
+        card_rarity = RRR
         pack_rarities.append(card_rarity)
         return pack_rarities
     else:
@@ -90,9 +98,9 @@ def open_pack():
             match rarity:
                 case x if x < 60:
                     card_rarity = C
-                case x if x < 90:
-                    card_rarity = R
                 case x if x < 95:
+                    card_rarity = R
+                case x if x < 99:
                     card_rarity = RR
                 case _:
                     card_rarity = RRR
@@ -113,18 +121,15 @@ def open_pack():
             pack_rarities.append(card_rarity)
         # at least one 'good' card in a pack
         rarity = random.uniform(0, 100)
-        if (pack_rarities.count(C) == 6):
-            card_rarity = -1
-            match rarity:
-                case x if x < 45 :
-                    card_rarity = R
-                case x if x < 85:
-                    card_rarity = RR
-                case x if x < 99.5:
-                    card_rarity = RRR
-                case _:
-                    card_rarity = SP
-            pack_rarities.append(card_rarity)
+        card_rarity = -1
+        match rarity:
+            case x if x < 40 :
+                card_rarity = R
+            case x if x < 85:
+                card_rarity = RR
+            case _:
+                card_rarity = RRR
+        pack_rarities.append(card_rarity)
         return pack_rarities
 
 def rarity_to_char(rarity):
@@ -137,7 +142,21 @@ def rarity_to_char(rarity):
     if rarity == RRR:
         return 'RRR'
     if rarity == SP:
-        return 'SP'
+        return 'RRR'
+    raise ValueError(f"Invalid rarity value: {rarity}")
+    
+def char_to_rarity(char):
+    if char == 'C':
+        return C
+    if char == 'R':
+        return R
+    if char == 'RR':
+        return RR
+    if char == 'RRR':
+        # Ambiguity resolution: Default to RRR unless specified otherwise
+        return RRR
+    raise ValueError(f"Invalid rarity character: {char}")
+
 
 def get_card(rarity):
     query = "SELECT * FROM cards WHERE rarity = %s AND number LIKE %s ORDER BY RAND() LIMIT 1;"
@@ -147,12 +166,38 @@ def get_card(rarity):
     card = cursor.fetchone()
     return card
 
-for i in range (0, 10):
+
+
+total_C = 0
+total_R = 0
+total_RR = 0
+total_RRR = 0
+
+test_amt = 20
+for i in range (0, test_amt):
+    print(f"\nOpening pack {i+1}!")
     card_rarities = open_pack()
+    total_C += card_rarities.count(C)
+    total_R += card_rarities.count(R)
+    total_RR += card_rarities.count(RR)
+    total_RRR += card_rarities.count(RRR)
+    
     for card_rarity in card_rarities:
         char_rarity = rarity_to_char(card_rarity)
         the_card = get_card(char_rarity)
-        print(the_card[1], the_card[17])
+        # print(the_card[1], the_card[17])
+
+
+prob_C = total_C / (test_amt * 7)* 100
+prob_R = total_R / (test_amt * 7)* 100
+prob_RR = total_RR / (test_amt * 7)* 100
+prob_RRR = total_RRR / (test_amt * 7)* 100
+
+print(f"Probability of Common (C): {prob_C:.2f}%\tProbability of Rare (R): {prob_R:.2f}%\tProbability of Double Rare (RR): {prob_RR:.2f}%\tProbability of Triple Rare (RRR): {prob_RRR:.2f}%")
+
+print(f"Collected {total_RRR} Triple Rare's (RRR)\n")
+    
+    
 # testing probabilities
 # total_C = 0
 # total_R = 0
