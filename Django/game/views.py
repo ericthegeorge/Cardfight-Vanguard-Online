1bb3b6c5d78dfc751440ef9b0a14a1f1f8e8a86f
Eric George <erickurian@gmail.com>
2025-01-02 22:20:57 -0700

    finished probs on packs and added flutter opening packs api endpoint

--- Django/game/views.py
+++ Django/game/views.py
@@ -4,16 +4,98 @@ from rest_framework.response import Response
 from .models import Cards
 from game.serializers import CardSerializer
 from random import sample
-
+import random
+SUPER_PACK_CHANCE = 0.5 # out of 100 in uniform random distribution
 # Create your views here.
 
 class PackOpenerView(APIView):
+    @staticmethod
+    def choose_pack_rarities():
+        pack_rarities = []
+        super_pack_rarity = random.uniform(0, 100)
+        if(super_pack_rarity < SUPER_PACK_CHANCE):
+            # print("SUPER PACK GET!")
+            # all cards are R or higher, and the last card is a RRR or higher
+            for i in range (0, 6):
+                rarity = random.uniform(0,100)
+                card_rarity = -1
+                match rarity:
+                    case x if x < 45:
+                        card_rarity = 'R'
+                    case x if x < 85:
+                        card_rarity = 'RR'
+                    case _:
+                        card_rarity = 'RRR'
+                pack_rarities.append(card_rarity)
+            rarity = random.uniform(0, 100)
+            card_rarity = 'RRR'
+            pack_rarities.append(card_rarity)
+            return pack_rarities
+        else:
+            # first four cards 90 % chance to be common
+            for i in range (0, 4):
+                rarity = random.uniform(0,100)
+                card_rarity = -1
+                match rarity:
+                    case x if x < 90:
+                        card_rarity = 'C'
+                    case x if x < 96:
+                        card_rarity = 'R'
+                    case x if x < 99:
+                        card_rarity = 'RR'
+                    case _:
+                        card_rarity = 'RRR'
+                pack_rarities.append(card_rarity)
+            # next two cards 50 % chance to be common
+            for i in range (4,6):
+                rarity = random.uniform(0, 100)
+                card_rarity = -1
+                match rarity:
+                    case x if x < 70:
+                        card_rarity = 'C'
+                    case x if x < 88:
+                        card_rarity = 'R'
+                    case x if x < 10:
+                        card_rarity = 'RR'
+                    case _:
+                        card_rarity = 'RRR'
+                pack_rarities.append(card_rarity)
+            # at least one 'good' card in a pack
+            rarity = random.uniform(0, 100)
+            card_rarity = -1
+            match rarity:
+                case x if x < 40 :
+                    card_rarity = 'R'
+                case x if x < 85:
+                    card_rarity = 'RR'
+                case _:
+                    card_rarity = 'RRR'
+            pack_rarities.append(card_rarity)
+            return pack_rarities
+    @staticmethod
+    def get_random_cards(rarities):
+        selected_cards = []
+        
+        for rarity in rarities:
+            all_cards = Cards.objects.filter(rarity= rarity, number__startswith="BT01")
+            total_cards = all_cards.count()
+            
+            rindx = random.randint(0, total_cards - 1)
+            card = all_cards[rindx]
+            selected_cards.append(card)
+        return selected_cards
+    
     def get(self, request):
-        card_ids = list(Cards.objects.values_list('id', flat=True))
-        random_ids = sample(card_ids, 7)
-        cards = Cards.objects.filter(id__in=random_ids)
+        
+        super_pack_rarity = random.uniform(0, 100)
+        pack_rarities = PackOpenerView.choose_pack_rarities()
+            # regular pack logic
+        # get cards at end based on rarities
+        cards = PackOpenerView.get_random_cards(pack_rarities)
         serializer = CardSerializer(cards, many=True)
         return Response(serializer.data)
+
+
 class AllCardsView(APIView):
     def get(self, request):
         cards = Cards.objects.all()
